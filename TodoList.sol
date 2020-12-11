da5500e706ff9c39ff6fdd643107bf5f0aac7ad0
Avinash Kumar <45197266+Avi-nashkumar@users.noreply.github.com>
2020-12-11 12:43:36 +0530

    Update TodoList.sol

--- TodoList.sol
+++ TodoList.sol
@@ -1,39 +1,48 @@
 pragma solidity ^0.5.1;
 
-contract TodoList {
-    uint public taskCount=0;
+contract TodoList{
     
-    struct Task{
+    uint public taskCount = 0;
+    
+    struct Task {
         uint id;
         string content;
         bool completed;
-        
-    }
-    mapping(uint=>Task) public tasks;
-    
-    //constructor
-    
-    event TaskCreated( uint id,string content, bool completed);
-    
-    event TaskCompleted(uint id, bool completed);
-    
-    //Fuctions to create TaskCompleted
-    
-    function createTask(string memory _content) public{
-        taskCount++;
-        tasks[taskCount]= Task(taskCount,_content,false);
-        emit TaskCreated(taskCount,_content,false);
-        
     }
     
-    function toggleCompleted(uint _id ) public{
+    mapping(uint => Task) public tasks;
+    
+    
+    constructor() public{
+        createTask("Todo App on Blockchain");
+    }
+    
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
+    
+    event TaskCompleted (
+        uint id,
+        bool completed
+        );
+    
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
+    }
+    
+    function toggleCompleted(uint _id) public {
         Task memory _task = tasks[_id];
         _task.completed = !_task.completed;
-        tasks[_id]= _task;
-        emit TaskCompleted(_id,_task.completed);
-    }
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
+    }   
+
     function getTask(uint _id) public view returns(string memory){
-        return(tasks[_id].content);
-    }
+       return(tasks[_id].content);
+    }  
 }
 
